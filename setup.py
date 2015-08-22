1b7fb5319547c77dcfc279bdac27efa37e1eff3b
Josiah Klassen <jkla@telus.net>
2015-08-21 23:25:28 -0700

    add basic django project (wsgi not hooked up)

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+setup(
+    name='skyworlds',
+    version='0.1',
+    description='OpenShift App for the SkyWolrds Game',
+    author='Josiah Klassen',
+    author_email='jkla@telus.net',
+    url='http://www.python.org/sigs/distutils-sig/',
+    install_requires=[
+        'Django>=1.8',
+        'psycopg2==2.6.1',
+    ],
+)
